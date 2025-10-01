cb685e791e71e90162a19d646b43271bec4ed3e4
dmytro_tov <dmitriy.tovstitsky@gmail.com>
2025-10-01 11:27:48 +0300

    Update LessonView.jsx

--- src/pages/LessonView.jsx
+++ src/pages/LessonView.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { ArrowLeft, CheckCircle, Award, AlertCircle } from 'lucide-react';
+import { completeLesson } from '../utils/progressManager';
 
 export default function LessonView() {
   const { day } = useParams();
